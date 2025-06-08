0357753f72b1b4d9dfc1dbd8bfa1a7d1b1836587
Victor <victorm.martinezjr@outlook.com>
2025-06-08 18:19:26 -0400

    add searchbar and quick links to page in SceneBuilder

--- src/main/java/in/victormartinezjr/buildinganavaid/NavAid.java
+++ src/main/java/in/victormartinezjr/buildinganavaid/NavAid.java
@@ -12,6 +12,7 @@ public class NavAid extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(NavAid.class.getResource("NavAid.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 1000);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setTitle("Nav Aid");
         stage.setScene(scene);
         stage.show();
